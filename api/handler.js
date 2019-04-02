c315f7d6ee7801cab10e15f1e291dc72dbc2725d
Bryan Hadaway <treatmentrecordsolutions@gmail.com>
2019-04-02 15:51:34 -0400

    successfully connected to db in getUsers funtion

--- api/handler.js
+++ api/handler.js
@@ -1,14 +1,67 @@
-'use strict';
+"use strict";
+const MongoClient = require('mongodb').MongoClient;
+const MONGODB_URI = "mongodb+srv://admin:Ululavit38@datingappcluster-y2wam.mongodb.net/test?retryWrites=true";
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+let cachedDb = null;
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
+function connectToDatabase (uri) {
+
+  console.log('=> connect to database');
+
+
+
+  if (cachedDb) {
+
+    console.log('=> using cached database instance');
+
+    return Promise.resolve(cachedDb);
+
+  }
+
+  const dbName = 'dating-app';
+  return MongoClient.connect(uri)
+  .then(client => { cachedDb = client.db(dbName); return cachedDb; });
+
+  // return MongoClient.connect(uri)
+ 
+    // .then(db => {
+
+    //   cachedDb = db;
+
+    //   return cachedDb;
+
+    // });
+
+}
+
+
+function queryDatabase (db) {
+  console.log('=> query database');
+
+  return db.collection('users').find({}).toArray()
+    .then(() => { return { statusCode: 200, body: 'success' }; })
+    .catch(err => {
+      console.log('=> an error occurred: ', err);
+      return { statusCode: 500, body: 'error' };
+    });
+}
+
+module.exports.getUsers = (event, context, callback) => {
+
+  context.callbackWaitsForEmptyEventLoop = false;
+
+
+  console.log('event: ', event);
+
+  connectToDatabase(MONGODB_URI)
+    .then(db => queryDatabase(db))
+    .then(result => {
+      console.log('=> returning result: ', result);
+      callback(null, result);
+    })
+    .catch(err => {
+      console.log('=> an error occurred: ', err);
+      callback(err);
+    });
 };
